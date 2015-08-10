492e180ed8a8ca7d6cb0fb7079d344144b50033d
Ben Scheirman <ben@scheirman.com>
2015-08-10 09:26:29 -0500

    Initial framework project

---
+++ AdzerkSDK/AdzerkSDK/AdzerkSDK.h
@@ -0,0 +1,19 @@
+//
+//  AdzerkSDK.h
+//  AdzerkSDK
+//
+//  Created by Ben Scheirman on 8/10/15.
+//  Copyright (c) 2015 Adzerk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdzerkSDK.
+FOUNDATION_EXPORT double AdzerkSDKVersionNumber;
+
+//! Project version string for AdzerkSDK.
+FOUNDATION_EXPORT const unsigned char AdzerkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdzerkSDK/PublicHeader.h>
+
+
