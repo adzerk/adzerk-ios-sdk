1d6a9ed76206fef208583c98a85d92eaab8f1600
Ben Scheirman <ben@scheirman.com>
2015-09-10 11:19:45 -0500

    Fix build script for Xcode 7 build tools

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ echo "[Building]"
 xctool -workspace $WORKSPACE -scheme $SCHEME -sdk iphoneos
 
 echo "[Testing]"
-# xctool -workspace $WORKSPACE -scheme $SCHEME -sdk iphonesimulator test
 # xctool currently shows an error when running tests, possibly due to this being a framework
 # we'll use xcodebuild instead and deal with less-than-readable output.
-xcodebuild -workspace $WORKSPACE -scheme $SCHEME test
+# Xcode 7 support for testing w/ xctool is coming: https://github.com/facebook/xctool/issues/528
+xcodebuild -workspace $WORKSPACE -scheme $SCHEME -sdk iphonesimulator test
 
 
