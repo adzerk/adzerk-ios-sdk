03e7e2a8a17d1a1801ae5d553f96d83f519a9766
Dave Yarwood <dave.yarwood@gmail.com>
2016-08-03 12:44:13 -0400

    add tests for ADZPlacement.serialize()

--- AdzerkSDK/AdzerkSDKTests/AdzerkSDKTests.swift
+++ AdzerkSDK/AdzerkSDKTests/AdzerkSDKTests.swift
@@ -14,36 +14,148 @@ class AdzerkSDKTests: XCTestCase {
     let networkId = 9792
     let siteId = 306998
     var sdk: AdzerkSDK!
-    
+
     override func setUp() {
         super.setUp()
         AdzerkSDK.defaultNetworkId = networkId
         AdzerkSDK.defaultSiteId = siteId
         sdk = AdzerkSDK()
-        
+
     }
-    
+
     override func tearDown() {
         super.tearDown()
     }
-    
+
     func testDefaultNetworkIdAndSiteId() {
         XCTAssertEqual(AdzerkSDK.defaultNetworkId!, networkId, "network id was not set")
         XCTAssertEqual(AdzerkSDK.defaultSiteId!, siteId, "site id was not set")
     }
-    
+
     func testCreatePlacementWithDivAndAdTypes() {
         let placement = ADZPlacement(divName: "asdf", adTypes: [])
         XCTAssertTrue(placement != nil, "was nil")
     }
-    
+
+    func testSerializePlacement() {
+        let placement = ADZPlacement(divName: "aoeu", adTypes: [5])
+
+        placement.zoneIds = [136961]
+        placement.properties = [
+            "some_key": "some_value",
+            "pets": [
+              [
+                "name":    "Steve",
+                "species": "cat",
+                "age":     12
+              ],
+              [
+                "name":    "Barbara"
+                "species": "dog",
+                "age":     7
+              ]
+            ]
+        ]
+
+        let json = placement.serialize()
+
+        XCTAssertTrue(placement != nil, "placement was nil")
+        XCTAssertTrue(json != nil, "json was nil")
+
+        if let divName = json["divName"] as? String {
+          XCTAssertEqual(divName, "aoeu", "expected divName to be \"aoeu\", got \"\(divName)\"")
+        } else {
+          XCTFail("Unable to read json.divName as a string.")
+        }
+
+        // set by default value on setup
+        if let pNetworkId = json["networkId"] as? Int {
+          XCTAssertEqual(pNetworkId, networkId, "expected networkId to be \(networkId), got \(pNetworkId)")
+        } else {
+          XCTFail("Unable to read json.networkId as an int.")
+        }
+
+        // set by default value on setup
+        if let pSiteId = json["siteId"] as? Int {
+          XCTAssertEqual(pSiteId, siteId, "expected siteId to be \(siteId), got \(pSiteId)")
+        } else {
+          XCTFail("Unable to read json.siteId as an int.")
+        }
+
+        if let adTypes = json["adTypes"] as? Array {
+          if let adType = adTypes[0] as? Int {
+            XCTAssertEqual(adType, 5, "expected adType to be 5, got \(adType)")
+          } else {
+            XCTFail("Unable to read json.adTypes[0] as an int.")
+          }
+        } else {
+          XCTFail("Unable to read json.adTypes as an array.")
+        }
+
+        if let zoneIds = json["zoneIds"] as? Array {
+          if let zoneId = zoneIds[0] as? Int {
+            XCTAssertEqual(zoneId, 136961, "expected zoneId to be 136961, got \(zoneId)")
+          } else {
+            XCTFail("Unable to read json.zoneIds[0] as an int.")
+          }
+        } else {
+          XCTFail("Unable to read json.zoneIds as an array.")
+        }
+
+        if let properties = json["properties"] as? [String: AnyObject] {
+          if let some_value = properties["some_key"] as? String {
+            XCTAssertEqual(some_value, "some_value", "expected \"some_value\", got \"\(some_value)\"")
+          } else {
+            XCTFail("Unable to read json.properties.some_key as a string.")
+          }
+
+          if let pets = properties["pets"] as? Array {
+            if let steve = pets[0] as? [String: anyObject] {
+              if let species = steve["species"] as? String {
+                XCTAssertEqual(species, "cat", "expected Steve's species to be \"cat\", got \"\(species)\"")
+              } else {
+                XCTFail("Unable to read properties.pets[0].species as a string.")
+              }
+
+              if let species = steve["age"] as? Int {
+                XCTAssertEqual(age, 12, "expected Steve's age to be 12, got \(age)")
+              } else {
+                XCTFail("Unable to read properties.pets[0].age as an int.")
+              }
+            } else {
+              XCTFail("Unable to read properties.pets[0] as an object.")
+            }
+
+            if let barbara = pets[1] as? [String: anyObject] {
+              if let species = barbara["species"] as? String {
+                XCTAssertEqual(species, "dog", "expected Barbara's species to be \"dog\", got \"\(species)\"")
+              } else {
+                XCTFail("Unable to read properties.pets[1].species as a string.")
+              }
+
+              if let species = barbara["age"] as? Int {
+                XCTAssertEqual(age, 7, "expected Barbara's age to be 7, got \(age)")
+              } else {
+                XCTFail("Unable to read properties.pets[1].age as an int.")
+              }
+            } else {
+              XCTFail("Unable to read properties.pets[1] as an object.")
+            }
+          } else {
+            XCTFail("Unable to read json.properties.pets as an array.")
+          }
+        } else {
+          XCTFail("Unable to read json.properties as an object.")
+        }
+    }
+
     // Assert that the API response is called and returns .Success
     // response data is ignored
     func assertSuccessfulResponse(expectation: XCTestExpectation) -> (ADZResponse) -> () {
         return assertResponse(expectation)
     }
-    
-    // Assert that the API response is called. Calls the validationHandler in the case of .Success for callers to 
+
+    // Assert that the API response is called. Calls the validationHandler in the case of .Success for callers to
     // validate the response structure.
     func assertResponse(expectation: XCTestExpectation, validationHandler: (ADZPlacementResponse -> ())? = nil) -> (ADZResponse -> ()) {
         return { (response) in
@@ -60,20 +172,20 @@ class AdzerkSDKTests: XCTestCase {
             expectation.fulfill()
         }
     }
-    
+
     func testCanRequestSimplePlacement() {
         let expectation = expectationWithDescription("API response received")
         sdk.requestPlacementInDiv("div1", adTypes: [5], completion: assertSuccessfulResponse(expectation))
         waitForExpectationsWithTimeout(3.0, handler: nil)
     }
-    
+
     func testCanRequestPlacementWithAdditionalParameters() {
         let placement = ADZPlacement(divName: "div1", adTypes: [])!
         let expectation = expectationWithDescription("Successful API Response received")
         sdk.requestPlacement(placement, completion: assertSuccessfulResponse(expectation))
         waitForExpectationsWithTimeout(3.0, handler: nil)
     }
-    
+
     func testCanRequestPlacementwithAllParameters() {
         let placement = ADZPlacement(divName: "div1", adTypes: [5])!
         placement.zoneIds = [136961]
@@ -94,7 +206,7 @@ class AdzerkSDKTests: XCTestCase {
                 XCTAssertTrue(dec.flightId != nil, "Flight id was not set")
                 XCTAssertTrue(dec.campaignId != nil, "Campaign id was not set")
                 XCTAssertTrue(dec.clickUrl != nil, "Click URL was not set")
-                
+
                 if let contents = dec.contents {
                     XCTAssertEqual(contents.count, 1, "Should have had 1 item in contents")
                     if let content = contents.first {
@@ -103,17 +215,17 @@ class AdzerkSDKTests: XCTestCase {
                         XCTAssertTrue(content.data != nil, "content data should have been set")
                         XCTAssertTrue(content.body != nil, "content body should have been set")
                     }
-                    
+
                 }
                 XCTAssertEqual((dec.events?.count)!, 0, "events should have been an empty array")
-                
+
             } else {
                 XCTFail("couldn't find div1 in response")
             }
         }))
         waitForExpectationsWithTimeout(3.0, handler: nil)
     }
-    
+
     func testCanRequestMultiplePlacements() {
         let placement1 = ADZPlacement(divName: "div1", adTypes: [5])!
         let placement2 = ADZPlacement(divName: "div2", adTypes: [5])!
@@ -124,7 +236,7 @@ class AdzerkSDKTests: XCTestCase {
         }))
         waitForExpectationsWithTimeout(3.0, handler: nil)
     }
-    
+
     func testCanRequestPlacementsWithOptions() {
         let placement1 = ADZPlacement(divName: "div1", adTypes: [5])!
         placement1.adId = 1
@@ -132,13 +244,13 @@ class AdzerkSDKTests: XCTestCase {
         placement1.flightId = 1
         placement1.eventIds = [123]
         placement1.properties = ["key":"val"]
-        
+
         let expectation = expectationWithDescription("API response received")
         let options = ADZPlacementRequestOptions()
         options.flightViewTimes = [
             "1234": [151243, 5124312]
         ]
-        
+
         options.blockedCreatives = [1,2,3]
         options.keywords = ["cheese", "apples", "wine"]
         sdk.requestPlacements([placement1], options: options,completion: assertResponse(expectation, validationHandler: { response in
@@ -146,36 +258,36 @@ class AdzerkSDKTests: XCTestCase {
         }))
         waitForExpectationsWithTimeout(3.0, handler: nil)
     }
-    
+
     func testSavesUserKey() {
         let fakeKeyStore = FakeKeyStore()
         let sdk = AdzerkSDK(userKeyStore: fakeKeyStore)
         let expectation = expectationWithDescription("API response received")
         sdk.requestPlacementInDiv("div1", adTypes: [5], completion: assertSuccessfulResponse(expectation))
         waitForExpectationsWithTimeout(3.0, handler: nil)
-        
+
         XCTAssertTrue(fakeKeyStore.key != nil, "User key was not set")
     }
-    
+
     func testSendsSavedUserKey() {
         let fakeKeyStore = FakeKeyStore()
         fakeKeyStore.key = "testkey12345"
-        
+
         let sdk = AdzerkSDK(userKeyStore: fakeKeyStore)
         let expectation = expectationWithDescription("API response received")
         sdk.requestPlacementInDiv("div1", adTypes: [5], completion: assertSuccessfulResponse(expectation))
         waitForExpectationsWithTimeout(3.0, handler: nil)
-        
+
         XCTAssertEqual(fakeKeyStore.currentUserKey()!, "testkey12345")
     }
-    
+
     func testCanPostUserProperties() {
         let properties = [
             "foo" : "bar",
             "isCustom": true,
             "numberOfGems": 25
         ]
-        
+
         let userKey = "userKey123"
         let expectation = expectationWithDescription("API response received")
         sdk.postUserProperties(userKey, properties: properties) { success, error in
@@ -183,10 +295,10 @@ class AdzerkSDKTests: XCTestCase {
             XCTAssertTrue(success)
             expectation.fulfill()
         }
-        
+
         waitForExpectationsWithTimeout(3.0, handler: nil)
     }
-    
+
     func testCanReadUser() {
         let userKey = "userKey123"
         let expectation = expectationWithDescription("API response received")
@@ -201,10 +313,10 @@ class AdzerkSDKTests: XCTestCase {
 
             expectation.fulfill()
         }
-        
+
         waitForExpectationsWithTimeout(3.0, handler: nil)
     }
-    
+
     func testCanAddInterest() {
         let userKey = "userKey123"
         let expectation = expectationWithDescription("API Response received")
@@ -213,10 +325,10 @@ class AdzerkSDKTests: XCTestCase {
             XCTAssertNil(error)
             expectation.fulfill()
         }
-        
+
         waitForExpectationsWithTimeout(3.0, handler: nil)
     }
-    
+
     func testCanOptOut() {
         let userKey = "userKey123"
         let expectation = expectationWithDescription("API response received")
@@ -227,7 +339,7 @@ class AdzerkSDKTests: XCTestCase {
         }
         waitForExpectationsWithTimeout(3.0, handler: nil)
     }
-    
+
     func testCanRetargetUser() {
         let userKey = "userKey123"
         let brandId = 88205
