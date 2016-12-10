f399ca1729b863c2b8c0b9cabe20a4c2170104f6
Ben Scheirman <ben@scheirman.com>
2016-12-09 19:38:33 -0600

    Add custom user-agent to all requests.
    Now formatted like:
    
    AdzerkSDK/1.1  ( ObjCExample/1.0 - iPhone 7/iOS 10.1 )
    
    The format of the string is
    - sdkname/version
    - appname/version
    - device platform / OS Version

--- AdzerkSDK/AdzerkSDK/AdzerkSDK.swift
+++ AdzerkSDK/AdzerkSDK/AdzerkSDK.swift
@@ -7,6 +7,10 @@
 //
 
 import Foundation
+import UIKit
+
+// Update this when making changes. Will ben sent in the UserAgent to identify the version of the SDK used in host applications.
+let AdzerkSDKVersion = 1.1
 
 /** The base URL to use for API requests. */
 let AdzerkBaseUrl = "https://engine.adzerk.net/api/v2"
@@ -16,6 +20,70 @@ public typealias ADZResponseSuccessCallback = (ADZPlacementResponse) -> ()
 public typealias ADZResponseFailureCallback = (Int?, String?, Error?) -> ()
 public typealias ADZResponseCallback = (Bool, Error?) -> ()
 public typealias ADZUserDBUserResponseCallback = (ADZUser?, Error?) -> ()
+
+
+// This provider object constructs the user agent only once, and is used repeatedly.
+fileprivate struct UserAgentProvider {
+    static var instance = UserAgentProvider()
+    
+    private init() {
+    }
+    
+    lazy var userAgent: String  = {
+        var string = "AdzerkSDK/\(AdzerkSDKVersion)"
+        let mainBundle = Bundle.main
+        
+        if let bundleName = mainBundle.object(forInfoDictionaryKey: "CFBundleName"),
+           let bundleVersion = mainBundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") {
+
+            let deviceName = self.deviceModelName
+            let osVersion = "\(UIDevice.current.systemName) \(UIDevice.current.systemVersion)"
+            string.append("  (\(bundleName)/\(bundleVersion) - \(deviceName)/\(osVersion)   )")
+        }
+        
+        return string
+    }()
+
+    var deviceModelName: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        switch identifier {
+        case "iPod5,1":                                 return "iPod Touch 5"
+        case "iPod7,1":                                 return "iPod Touch 6"
+        case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return "iPhone 4"
+        case "iPhone4,1":                               return "iPhone 4s"
+        case "iPhone5,1", "iPhone5,2":                  return "iPhone 5"
+        case "iPhone5,3", "iPhone5,4":                  return "iPhone 5c"
+        case "iPhone6,1", "iPhone6,2":                  return "iPhone 5s"
+        case "iPhone7,2":                               return "iPhone 6"
+        case "iPhone7,1":                               return "iPhone 6 Plus"
+        case "iPhone8,1":                               return "iPhone 6s"
+        case "iPhone8,2":                               return "iPhone 6s Plus"
+        case "iPhone9,1", "iPhone9,3":                  return "iPhone 7"
+        case "iPhone9,2", "iPhone9,4":                  return "iPhone 7 Plus"
+        case "iPhone8,4":                               return "iPhone SE"
+        case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return "iPad 2"
+        case "iPad3,1", "iPad3,2", "iPad3,3":           return "iPad 3"
+        case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad 4"
+        case "iPad4,1", "iPad4,2", "iPad4,3":           return "iPad Air"
+        case "iPad5,3", "iPad5,4":                      return "iPad Air 2"
+        case "iPad2,5", "iPad2,6", "iPad2,7":           return "iPad Mini"
+        case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad Mini 2"
+        case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad Mini 3"
+        case "iPad5,1", "iPad5,2":                      return "iPad Mini 4"
+        case "iPad6,3", "iPad6,4", "iPad6,7", "iPad6,8":return "iPad Pro"
+        case "AppleTV5,3":                              return "Apple TV"
+        case "i386", "x86_64":                          return "Simulator"
+        default:                                        return identifier
+        }
+    }
+}
 
 /** The primary class used to make requests against the API. */
 @objc open class AdzerkSDK : NSObject {
@@ -67,9 +135,9 @@ public typealias ADZUserDBUserResponseCallback = (ADZUser?, Error?) -> ()
     /** Requests placements with explicit success and failure callbacks. Provided for Objective-C compatibility.
         See `requestPlacements:options:completion` for complete documentation.
     */
-    open func requestPlacements(_ placements: [ADZPlacement], options: ADZPlacementRequestOptions? = nil,
+    public func requestPlacements(_ placements: [ADZPlacement], options: ADZPlacementRequestOptions? = nil,
         success: @escaping (ADZPlacementResponse) -> (),
-        failure: @escaping (Int?, String?, Error?) -> ()) {
+        failure: @escaping (Int, String?, NSError?) -> ()) {
         
         requestPlacements(placements, options: options) { response in
             switch response {
@@ -78,9 +146,9 @@ public typealias ADZUserDBUserResponseCallback = (ADZUser?, Error?) -> ()
             case .badRequest(let statusCode, let body):
                 failure(statusCode, body, nil)
             case .badResponse(let body):
-                failure(nil, body, nil)
+                failure(0, body, nil)
             case .error(let error):
-                failure(nil, nil, error)
+                failure(0, nil, error as NSError)
             }
         }
     }
@@ -483,6 +551,7 @@ public typealias ADZUserDBUserResponseCallback = (ADZUser?, Error?) -> ()
     lazy var sessionConfiguration: URLSessionConfiguration = {
         let config = URLSessionConfiguration.ephemeral
         config.httpAdditionalHeaders = [
+            "User-Agent" : UserAgentProvider.instance.userAgent,
             "Content-Type" : "application/json",
             "Accept" : "application/json"
         ]
