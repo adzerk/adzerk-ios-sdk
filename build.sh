bedf3566b5167f05ff80f27691a3b0e393441b74
Ben Scheirman <ben@scheirman.com>
2017-02-28 08:57:54 -0600

    update test simulator device to 10.2

--- build.sh
+++ build.sh
@@ -20,4 +20,4 @@ echo "[Building]"
 xcodebuild -workspace $WORKSPACE -scheme $SCHEME -sdk iphonesimulator
 
 echo "[Testing]"
-xcodebuild -workspace $WORKSPACE -scheme $SCHEME -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7,OS=10.1" test | $output_command
+xcodebuild -workspace $WORKSPACE -scheme $SCHEME -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7,OS=10.2" test | $output_command
