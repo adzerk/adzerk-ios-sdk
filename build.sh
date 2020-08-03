a65054712e5a1f8b00bc01963547c9ff3fd23bb6
Ben Scheirman <ben@scheirman.com>
2020-08-03 09:39:02 -0500

    Build for Xcode 11 / iOS 13.5

--- build.sh
+++ build.sh
@@ -22,5 +22,5 @@ xcodebuild -workspace $WORKSPACE -scheme $SCHEME -sdk iphonesimulator
 echo "[Testing]"
 xcodebuild -workspace $WORKSPACE -scheme $SCHEME \
   -sdk iphonesimulator \
-  -destination "platform=iOS Simulator,name=iPhone 11 Pro,OS=13.3" \
+  -destination "platform=iOS Simulator,name=iPhone 11 Pro,OS=13.5" \
   test | $output_command
