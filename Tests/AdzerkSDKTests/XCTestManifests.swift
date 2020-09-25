dbe39dd45c9c15885b03fc55e009a1348d36d233
Ben Scheirman <ben@scheirman.com>
2020-09-25 13:47:01 -0500

    placement with additional options, codable

--- Tests/AdzerkSDKTests/XCTestManifests.swift
+++ Tests/AdzerkSDKTests/XCTestManifests.swift
@@ -5,7 +5,8 @@ public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(AdzerkSDKTests.allTests) +
         testCase(LoggerTests.allTests) +
-        testCase(UserKeyStoreKeychainTests.allTests)
+        testCase(UserKeyStoreKeychainTests.allTests) +
+        testCase(PlacementTests.allTests)
     ]
 }
 #endif
