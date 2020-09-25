b2ef0c36c17fb0840c2c7fe2aac7ead8cc70494c
Ben Scheirman <ben@scheirman.com>
2020-09-25 11:03:19 -0500

    Add keychain user key store

--- Tests/AdzerkSDKTests/XCTestManifests.swift
+++ Tests/AdzerkSDKTests/XCTestManifests.swift
@@ -3,7 +3,9 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(AdzerkSDKTests.allTests),
+        testCase(AdzerkSDKTests.allTests) +
+        testCase(LoggerTests.allTests) +
+        testCase(UserKeyStoreKeychainTests.allTests)
     ]
 }
 #endif
